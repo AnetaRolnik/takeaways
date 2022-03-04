8ff53436c89d318088b38394aea260aeddeef94e
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-03-04 08:23:13 +0100

    Add Meals component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import Hero from "./components/Layout/Hero";
+import Meals from "./components/Meals/Meals";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
       <Hero />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 };
