d56809ef4e26ce552a3a6f3c900b1966e0fbe6a8
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-03-04 08:37:22 +0100

    Visual improvements

--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -1,6 +1,8 @@
 import heroImage from "../../assets/hero.jpg";
 import classes from "./Hero.module.css";
 
-const Hero = () => <img src={heroImage} className={classes["hero-img"]} />;
+const Hero = () => (
+  <img src={heroImage} className={classes["hero-img"]} alt="Takeaways" />
+);
 
 export default Hero;
