e5fb1dbe4ab3cde51212486506473a41f4d6ee7e
Aneta Rolnik <aneta.r@poczta.onet.eu>
2022-05-06 20:31:41 +0200

    Add Lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import React, { useState, Suspense } from "react";
 
 import classes from "./App.module.css";
 import Header from "./components/Layout/Header";
 import Hero from "./components/Layout/Hero";
 import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import Loader from "./components/UI/Loader";
+
+const Cart = React.lazy(() => import("./components/Cart/Cart"));
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,7 +17,9 @@ const App = () => {
 
   return (
     <CartProvider>
-      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Suspense fallback={<Loader />}>
+        {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      </Suspense>
       <Header onShowCart={showCartHandler} />
       <Hero />
       <main className={classes.container}>
